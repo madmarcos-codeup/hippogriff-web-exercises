0681a9dd45249c8c04d17927c9e2762d661b4357
Javier Ruedas <jruedas@twc.com>
2023-09-08 10:49:09 -0500

    complete JS with HTML lecture

--- js/javascript-with-html.js
+++ js/javascript-with-html.js
@@ -6,7 +6,7 @@ let myNumber = 7;
 
 // alert("Hello, I am an alert!");
 
-/*==== PROMPT ====== */
+/*==== CONFIRM ====== */
 
 // let userChoice = confirm("Wait! Don't leave yet! Can I chat with you!");
 //
@@ -15,4 +15,22 @@ let myNumber = 7;
 // } else {
 //     console.log("boooo");
 // }
+
+/*==== PROMPT ====== */
+
+let currentSpeed = prompt("How fast are you driving?");
+let plannedSpeed = prompt("How fast do you want to drive?");
+let distanceLeft = prompt("How many miles do you have left?");
+
+let timeAtCurrentSpeed = (distanceLeft/currentSpeed) * 60;
+
+let timeAtPlannedSpeed = (distanceLeft/plannedSpeed) * 60;
+
+let timeSaved = timeAtCurrentSpeed - timeAtPlannedSpeed;
+
+// Output with template literals
+console.log(`At your current speed you will take ${timeAtCurrentSpeed.toFixed(0)} minutes. At your planned speed you will take ${timeAtPlannedSpeed.toFixed(0)} minutes. You will save ${timeSaved} minutes.`);
+
+// Output with concatenation
+console.log("At your current speed you will take " + timeAtCurrentSpeed + " minutes. At your planned speed you will take " + timeAtPlannedSpeed + " minutes. You will save " + timeSaved + " minutes.")
 
