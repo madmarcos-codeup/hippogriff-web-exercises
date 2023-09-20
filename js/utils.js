ede398ca60a7671da34edbf504084dc5f8eda6fe
Javier Ruedas <jruedas@twc.com>
2023-09-20 10:56:30 -0500

    add function to utils

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,11 @@ function isNumber(input){
         && typeof input !== "undefined"
         && !isNaN(input))
 }
+
+function isNumberOrNumericString(input){
+    return (typeof input !== "boolean"
+        && typeof input !== "object"
+        && typeof input !== "undefined"
+        && !isNaN(parseFloat(input))
+    );
+}
