10ce0137af5f54dc3aca3c35c2b0e66dcb28abd5
madmarcos-codeup <mark.robinson@codeup.com>
2023-10-20 11:16:22 -0500

    starter walkthru for request exercise

--- js/fetch.js
+++ js/fetch.js
@@ -7,11 +7,30 @@ const ghOptions = {
     }
 }
 
-fetch("https://api.github.com/users", ghOptions)
-    .then(response => response.json())
-    .then(data => console.log(data));
+// fetch("https://api.github.com/users", ghOptions)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
 
+function getGHUserLastCommitDate(userName) {
+    return fetch(`https://api.github.com/users/${userName}/events/public`, ghOptions)
+        .then(response => response.json())
 
-fetch("https://api.github.com/users/madmarcos-codeup/events/public", ghOptions)
-    .then(response => response.json())
-    .then(data => console.log(data));
+}
+
+getGHUserLastCommitDate("madmarcos-codeup")
+    .then(data => {
+            // console.log(data)
+            // 1. iterate through the event array
+            for (let i = 0; i < data.length; i++) {
+                // console.log(data[i]);
+                // 2. when we find the first PushEvent, return that created_at
+                if(data[i].type === "PushEvent") {
+                    console.log("last commit " + data[i].created_at);
+                    break;
+                }
+            }
+            // 3. if we got here, did not find a pushevent
+            // so return undefined
+            console.log("user must be lazy");
+    });
+console.log(getGHUserLastCommitDate("mojombo"));
